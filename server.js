af6e6f59a72b7cb7212ce0407e71cd0cc1eb8518
Dharani2156 <dharanikumar21506@gmail.com>
2025-07-12 09:42:57 +0530

    Fix: use numeric MySQL port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
+    port: Number(process.env.MYSQLPORT),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
